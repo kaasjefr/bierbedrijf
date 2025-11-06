998d53a4de11b2970f0dbbe012f00db5167b3095
TonkOnCrack <benjaminkaper@gmail.com>
2025-11-06 08:58:44 +0100

    kekge

--- bestellingbekijken.php
+++ bestellingbekijken.php
@@ -2,18 +2,15 @@
 
 ?>
 
-
 <!DOCTYPE html>
-<html>
-<a href=""></a>
-
-
-
-
-
-
-
-
-
-
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="/bestellingweigeren.php">weigeren</a>
+    <a href="/bestellingaccepteren.php">accepteren</a>
+</body>
 </html>
