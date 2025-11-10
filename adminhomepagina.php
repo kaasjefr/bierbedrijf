3bb2dbc83b1daa8bc18f8ca89594ec0fbceeb1e8
Leon <Leonnwlnd@gmail.com>
2025-11-10 13:32:53 +0100

    Update adminhomepagina.php

--- adminhomepagina.php
+++ adminhomepagina.php
@@ -13,9 +13,7 @@ require __DIR__ . '/config.php';
   <p>Ingelogd als: <?php echo htmlspecialchars($_SESSION['gebruikers']['email']); ?> (<?php echo $_SESSION['gebruikers']['rol']; ?>)</p>
   <ul>
     <li><a href="bestellingbekijken.php">Bestellingen</a></li>
-    <li><a href="bestellingaccepteren.php">Bestellingen Accepteren</a></li>
     <li><a href="bestellingbekijken.php">Bestelling bekijken</a></li>
-    <li><a href="bestellingweigeren.php">Bestelling weigeren</a></li>
   </ul>
   <!--<p><a href="index.php">Uitloggen</a></p>-->
 </body>
