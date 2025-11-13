9eadd6a0d38d10d8f31bef7e3aa8c7e5b313fdbc
TonkOnCrack <benjaminkaper@gmail.com>
2025-11-13 14:17:58 +0100

    fuck

--- bestellingweigeren.php
+++ bestellingweigeren.php
@@ -5,13 +5,14 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
 
-Bestelling geweigerd
+<body>
 </body>
+
 </html>
--- bestellingwijzigen.php
+++ bestellingwijzigen.php
@@ -17,19 +17,17 @@
 
 
 ?>
-
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>besteling wijzigen</title>
+</head>
 
+<body>
 
-
-
-
-
-
-
-
+</body>
 
 </html>
