b2eb7bcfe007e12306564776615dd76f0b300c51
Leon <Leonnwlnd@gmail.com>
2025-11-25 11:49:18 +0100

    Update bierbestellen.php

--- bierbestellen.php
+++ bierbestellen.php
@@ -5,25 +5,27 @@
 
   <h1>Bestelling plaatsen</h1>
   <form action="intotaal.php" method="post">
-    <div class="bestel-box">
-      <label for="flesjes_aantal">Aantal flesjes (minimaal 10):</label>
-      <input type="number" name="flesjes_aantal" id="flesjes_aantal" min="0" value="0" max="23">
-    </div>
 
-    <div class="bestel-box">
-      <label for="kratten_aantal">Aantal kratten (24 flesjes):</label>
-      <input type="number" name="kratten_aantal" id="kratten_aantal" min="0" value="0" max="23">
-    </div>
+      <label for="flesjes_aantal">
+      Aantal flesjes (minimaal 10):
+        <input type="number" name="flesjes_aantal" id="flesjes_aantal" min="0" value="0" max="23">
+      </label><br><br>
 
-    <div class="bestel-box">
-      <label for="pallets_aantal">Aantal pallets (24 kratten):</label>
-      <input type="number" name="pallets_aantal" id="pallets_aantal" min="0" value="0">
-    </div>
 
-    <div class="bestel-box">
-      <label for="leverdatum">Kies een leverdatum:</label>
-      <input type="date" id="leverdatum">
-    </div>
+      <label for="kratten_aantal">Aantal kratten (24 flesjes):
+        <input type="number" name="kratten_aantal" id="kratten_aantal" min="0" value="0" max="23">
+      </label><br><br>
+
+
+      <label for="pallets_aantal">Aantal pallets (24 kratten):
+        <input type="number" name="pallets_aantal" id="pallets_aantal" min="0" value="0">
+      </label><br><br>
+
+
+      <label for="leverdatum">Kies een leverdatum:
+        <input type="date" min="<?= date(format: "Y-m-d")?>">
+      </label><br><br><br><br>
+
 
 
     <button><a href="intotaal.php">Verder met bestellen</a></p></button>
